bb0ab773a7fe18273d87df058173fe3439e961af
Jason Livesay <ithkuil@gmail.com>
2013-05-26 01:22:20 -0600

    Create initscene.js

---
+++ initscene.js
@@ -0,0 +1,24 @@
+//three.min.js
+
+renderer = new THREE.WebGLRenderer();
+renderer.setSize( $(window).width()-50, $(window).height()-50 );
+document.body.appendChild( renderer.domElement );
+
+scene = new THREE.Scene();
+
+camera = new THREE.PerspectiveCamera(
+  35,         // Field of view
+  800 / 640,  // Aspect ratio
+  .1,         // Near
+  10000       // Far
+);
+
+camera.position.set( -15, 10, 15 );
+camera.lookAt( scene.position );
+
+var light = new THREE.PointLight( 0xFFFF00 );
+light.position.set( 10, 0, 10 );
+scene.add( light );
+
+renderer.render( scene, camera );
+
