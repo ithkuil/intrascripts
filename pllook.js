90fd4267954b17a32e0159f1a698cb5f16cf826c
Jason Livesay <ithkuil@gmail.com>
2013-06-08 19:23:14 -0700

    update

---
+++ pllook.js
@@ -0,0 +1,29 @@
+define( ['pointerlock', 'world'], function(pl, world) {
+  addType('looker', function() {
+    window.addEventListener('hookControls', function(camera) {
+      window.document.addEventListener 'mousemove', onMouseMove, false
+      var self = this;
+
+      this.pitchObject = new THREE.Object3D()  
+      this.pitchObject.add camera 
+
+      this.yawObject = new THREE.Object3D()
+      this.yawObject.position.y = 10
+      this.yawObject.add this.pitchObject;
+
+      this.PI_2 = Math.PI / 2;
+
+      onMouseMove = function (event) {
+        //if (!self.enabled) return;
+
+        @movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
+        @movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
+
+        self.yawObject.rotation.y -= self.movementX * 0.002;
+        self.pitchObject.rotation.x -= self.movementY * 0.002;
+
+        self.pitchObject.rotation.x = Math.max(- @PI_2, Math.min( @PI_2, self.pitchObject.rotation.x ));
+      });
+    });  
+  })
+});
